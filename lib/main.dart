eb90646516943b96ae27976386b7322373b9c33c
pongpak726 <fuza2560@gmail.com>
2025-12-12 16:21:34 +0700

    lab03

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/Model/recipe.dart';
+import 'package:recipes/recipe-detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -12,10 +13,7 @@ class RecipeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'Recipe calculator'),
     );
   }
@@ -23,8 +21,7 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
+  
   final String title;
 
   @override
@@ -32,33 +29,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: SafeArea(child: Container(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: SafeArea(
+        child: Container(
           child: ListView.builder(
-            itemBuilder: (BuildContext context,int index)
-            {
-            return buildRecipeCard(Recipe.samples[index]);
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  print((Recipe.samples[index].imgLabel));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      },
+                    ),
+                  );
+                },
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
             },
-            itemCount: Recipe.samples.length
-            ),
-        )),
-      );
-    }
+            itemCount: Recipe.samples.length,
+          ),
+        ),
+      ),
+    );
   }
+}
 
-  Widget buildRecipeCard(Recipe recipe){
-        return Card(
-          child: Column(
-            children: <Widget>[
-              Image(image: AssetImage(recipe.imageUrl)),
-              Text(recipe.imgLabel),
-            ]
-          )
-        );
-      }
-     
-  
-
+Widget buildRecipeCard(Recipe recipe) {
+  return Card(
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            recipe.imgLabel,
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 12.0),
+          Image(image: AssetImage(recipe.imageUrl)),
+          SizedBox(height: 14.0),
+          Text("I'm hungry"),
+        ],
+      ),
+    ),
+  );
+}
---
+++ lib/recipe-detail.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/Model/recipe.dart';
+
+class RecipeDetail extends StatelessWidget{
+ Recipe recipe;
+
+  RecipeDetail({super.key, required this.recipe});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe Detail'),
+      ),
+      body: Center(
+         child: Column(
+        children: [
+        Image.asset(recipe.imageUrl),
+        const SizedBox(height: 20.0,),
+        Text(
+          recipe.imgLabel,
+          style: const TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold
+          ),
+        )
+        ]
+         )
+      ),
+    );
+  }
+ 
+}
